343164ec79eba73ef7499b295054e6d551f73ae5
daydrilling <daydrilling@gmail.com>
2021-08-28 12:54:44 +0900

    preprocessing

---
+++ utils.py
@@ -0,0 +1,6 @@
+import os
+
+
+RAW_DATA_DIR_4 = '/data/speechdb/aihub_emotion/data/' # 4차년도
+RAW_DATA_DIR_51 = '/data/speechdb/aihub_emotion/5_years/5_wav/' # 5차년도
+RAW_DATA_DIR_52 = '/data/speechdb/aihub_emotion/5_years_2/' # 5차년도_2
